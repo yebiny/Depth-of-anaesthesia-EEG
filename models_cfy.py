cb7087cc370070166971fbdf7fa46467dbf2ed0e
yebiny <yebinyoun@gmail.com>
2021-04-14 10:41:25 +0900

    add model

--- models_cfy.py
+++ models_cfy.py
@@ -37,8 +37,52 @@ def cfy1(xshape, n_class, optimizer):
     model.compile(optimizer, loss, metrics=['accuracy'])    
     return model
 
+
+def cfy2(xshape, n_class, optimizer):
+    
+    model = models.Sequential()
+    model.add(layers.Input(shape=xshape))
+    
+    for rate in (1,2,4,8):
+        model.add(layers.Conv1D(filters=20, kernel_size=2, 
+                                padding='causal', activation='relu', dilation_rate=rate))
+    
+    model.add(layers.AveragePooling1D(50, padding='same'))
+    model.add(layers.Conv1D(20, 100, padding='same', activation='relu'))
+    
+    model.add(layers.Conv1D(1, 100, padding='same', activation='relu'))
+    model.add(layers.AveragePooling1D(10, padding='same'))
+    
+    if n_class==2:
+        n_dim = n_class-1
+        act = 'sigmoid'
+        loss='binary_crossentropy'
+    else:
+        n_dim = n_class
+        act = 'softmax'
+        loss='sparse_categorical_crossentropy'
+    
+    model.add(layers.Conv1D(n_dim*4, 10, padding='same'))
+    model.add(layers.AveragePooling1D(10, padding='same'))
+    model.add(layers.Reshape((n_dim*4, )))
+    
+    model.add(layers.Dense(n_dim))
+    model.add(layers.Activation(act))
+
+    model.compile(optimizer, loss, metrics=['accuracy'])    
+    return model
+
+def cfy3(xshape, n_class, optimizer):
+    model = models.Sequential()
+    model.add(layers.Input(shape=xshape))
+
+    model.add(layers.Dense(n_dim))
+    model.add(layers.Activation(act))
+
 MODELS = {
     'cfy1': cfy1,
+    'cfy2': cfy2,
+    'cfy3': cfy3,
 }
 
 
